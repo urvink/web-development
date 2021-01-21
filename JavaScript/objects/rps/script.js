e4f160d98a6fbb05d8acec399c98aa2e39d7eac4
Urvin <urvinkirindongo@gmail.com>
2021-01-21 17:19:23 +0100

    RPS & Objects | JS - Update

---
+++ JavaScript/objects/rps/script.js
@@ -0,0 +1,67 @@
+console.log("HOWDY");
+
+// const paper = 0;
+// const rock = 1;
+// const scissor = 2;
+
+
+
+function start(player1, computer) {
+    console.log("Start game");
+    game = new Game();
+}
+
+/**
+ * img
+ * id
+ * 
+ */
+class Game{
+    constructor(rock, paper, scissor){
+        this.p1 = new Player;
+        this.pc = new Player;
+        this.rock = rock;
+        this.paper = paper;
+        this.scissor = scissor;
+
+        function checkWinner(p1, pc) {
+            switch (p1) {
+                case rock:
+                    if (pc === scissor) {
+                        return `${p1} WINS!`;
+                    }else if (pc === p1 ){
+                        return `${p1} DRAW ${pc}`;
+                    }
+                    break;
+                case paper:
+                    if (pc === rock) {
+                        return `${p1} WINS!`;
+                    }
+                    break;
+                case scissor:
+                    if (pc === rock) {
+                        return `${p1} WINS!`;
+                    }
+                    break;
+                default:
+                    return
+                    break;
+            }
+            
+
+            document.getElementById('msg') = checkWinner(p1, pc);
+        }
+    }
+}
+
+class GO{
+    constructor(id, go_obj){
+        this.id = id;
+        this.go_obj = go_obj;
+    }
+}
+
+const paper = new GO("✋");
+const rock = new GO("🗿");
+const scissor = new GO("✂️");
+
