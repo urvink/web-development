4812dec34242ca829dc57b0ea30043f51ab5ef54
Urvin <urvinkirindongo@gmail.com>
2021-03-01 01:45:45 +0100

    Changelog * Clean up code * Added values for 'volgende klas?'

---
+++ Java/Syntax/Opdracht8.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class Opdracht8{
+    public static void opdracht8(String[] args) {
+        String[] eastcars = {"Kia, Honda"};
+        String[] cars = {"Kia", "Volkswagen", "Renault", "Honda", "BMW"};
+        System.out.println(cars);
+        // ArrayList<String> westcars = new ArrayList<>();
+    }
+}
